c29962366a35b3da6e3aeca9b75dde799803ddf0
Angela Simpson <angela@Administrators-MacBook-Air.local>
2013-10-19 19:06:44 -0700

    added test class

---
+++ gitTest/asTest.h
@@ -0,0 +1,13 @@
+//
+//  asTest.h
+//  gitTest
+//
+//  Created by Angela Simpson on 10/19/13.
+//  Copyright (c) 2013 Angela Simpson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface asTest : NSObject
+
+@end
---
+++ gitTest/asTest.m
@@ -0,0 +1,13 @@
+//
+//  asTest.m
+//  gitTest
+//
+//  Created by Angela Simpson on 10/19/13.
+//  Copyright (c) 2013 Angela Simpson. All rights reserved.
+//
+
+#import "asTest.h"
+
+@implementation asTest
+
+@end
